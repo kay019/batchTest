b03269ee332fc242748d38b1bac7eb35720283ab
kay019 <crowruya@gmail.com>
2022-04-18 01:34:22 +0000

    batch test

---
+++ src/main/java/com/batch/batchtest/BatchTestApplication.java
@@ -0,0 +1,14 @@
+package com.batch.batchtest;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableBatchProcessing
+@SpringBootApplication
+public class BatchTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/batch/batchtest/entity/Market.java
@@ -0,0 +1,26 @@
+package com.batch.batchtest.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Market {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int price;
+
+    @Override
+    public String toString(){
+        return "Market [id = " + id + " , name = " + name + " , price = " + price + " ]";
+    }
+}
